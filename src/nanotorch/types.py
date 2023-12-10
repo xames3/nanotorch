34f786d4d3d941b80a51861690ddb40ddcb05716
Akshay "XA" Mestry <xa@mes3.dev>
2023-12-09 19:35:30 -0600

    style: add separate types support
    Signed-off-by: Akshay "XA" Mestry <xa@mes3.dev>

---
+++ src/nanotorch/types.py
@@ -0,0 +1,19 @@
+"""\
+NanoTorch Types
+===============
+
+Author: Akshay Mestry <xa@mes3.dev>
+Created on: Saturday, December 09 2023
+Last updated on: Saturday, December 09 2023
+
+See https://github.com/xames3/nanotorch/ for more help.
+
+:copyright: (c) 2023 Akshay Mestry (XAMES3). All rights reserved.
+:license: MIT, see LICENSE for more details.
+"""
+
+import typing as t
+
+Colors = tuple[str, ...]
+Number = int | float | bool
+Size = t.Sequence[int]
