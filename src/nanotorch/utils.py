c1e17949d3a0e0cb513ea030a966509b8e393a81
Akshay "XA" Mestry <xa@mes3.dev>
2024-03-08 16:47:09 -0600

    chore: update version and year
    Signed-off-by: Akshay "XA" Mestry <xa@mes3.dev>

--- src/nanotorch/utils.py
+++ src/nanotorch/utils.py
@@ -8,7 +8,7 @@ Last updated on: Saturday, December 09 2023
 
 See https://github.com/xames3/nanotorch/ for more help.
 
-:copyright: (c) 2023 Akshay Mestry (XAMES3). All rights reserved.
+:copyright: (c) 2024 Akshay Mestry (XAMES3). All rights reserved.
 :license: MIT, see LICENSE for more details.
 """
 
