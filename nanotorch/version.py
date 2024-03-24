360aab24e96746779d88685f071232ab7f679d54
Akshay "XA" Mestry <xa@mes3.dev>
2024-03-24 11:40:45 -0500

    build: update installation
    Signed-off-by: Akshay "XA" Mestry <xa@mes3.dev>

--- nanotorch/version.py
+++ nanotorch/version.py
@@ -15,4 +15,4 @@ See https://github.com/xames3/nanotorch/ for more help.
 # According to Semantic Versioning (SemVer), major version zero (0.y.z)
 # is for initial development. Anything MAY change at any time.
 # The public API SHOULD NOT be considered stable.
-__version__: str = "2.0.0"
+__version__: str = "2.1.0"
